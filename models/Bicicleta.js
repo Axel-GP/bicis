db0561ad8ce5caf346a3d01667987e335054568d
Axel <axelgonzalezp82@gmail.com>
2026-03-06 20:39:28 +0000

    first commit

---
+++ models/Bicicleta.js
@@ -0,0 +1,33 @@
+let Bicicleta = function (id, color, modelo, ubicacion) {
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.allBicis = [];
+
+Bicicleta.add = function (bici) {
+    this.allBicis.push(bici);
+}
+
+
+let a = new Bicicleta(1, "Rojo", "Trek", [28.503789, -13.853296]);
+let b = new Bicicleta(2, "Azul", "Orbea", [28.501367, -13.853476]);
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+Bicicleta.removeById = function(aBiciId) {
+    for (let i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciId) {
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+};
+
+Bicicleta.findById = function(aBiciId) {
+    return Bicicleta.allBicis.find(x => x.id == aBiciId);
+};
+
+module.exports = Bicicleta;
